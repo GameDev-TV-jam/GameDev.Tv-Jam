c5a9e5f699087eab681945dbbca8ca65cb833f08
BenjaminLesar <sonofolorus@yahoo.com>
2022-05-19 19:40:11 -0700

    Start Menu Added

---
+++ Assets/Scripts/StartandCredits.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartandCredits : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void SeeCredits()
+    {
+        transform.GetChild(0).gameObject.SetActive(false);
+        transform.GetChild(1).gameObject.SetActive(true);
+
+    }
+
+    public void BacktoMain()
+    {
+        transform.GetChild(1).gameObject.SetActive(false);
+        transform.GetChild(0).gameObject.SetActive(true);
+    }
+}
